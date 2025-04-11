226ba646c9d038c6c9b41942e7932f443529be34
PYY-CREATER1 <3131228767@qq.com>
2025-04-11 22:33:32 +0800

    员工分页查询和禁用员工账户的代码

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -3,18 +3,18 @@ package com.sky.controller.admin;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -75,10 +75,25 @@ public class EmployeeController {
     }
 
     //添加员工
-    @PostMapping
-    public Result save(@RequestBody EmployeeDTO employeeDTO) {
-        log.info("新增员工: {}", employeeDTO);
-        employeeService.save(employeeDTO);
+//    @PostMapping
+//    public Result save(@RequestBody EmployeeDTO employeeDTO) {
+//        log.info("新增员工: {}", employeeDTO);
+//        employeeService.save(employeeDTO);
+//        return Result.success();
+//    }
+    /**
+     *员工分页查询
+     */
+    @GetMapping("/page")
+    public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO){
+        log.info("员工分页查询:{}",employeePageQueryDTO);
+        PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
+        return Result.success(pageResult);
+    }
+    //
+    @PostMapping("status/{status}")
+    public Result startOrStop(@PathVariable Integer status, long id){
+        employeeService.startOrStop(status,id);
         return Result.success();
     }
 
